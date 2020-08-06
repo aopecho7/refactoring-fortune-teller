5e40070926c8b8ec9a53a3327cf8d97c4924b2b1
aopecho <aopecho@uchicago.edu>
2020-08-06 00:12:02 +0000

    who knows

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # PART 2: RCAV DEBUGGING
   # ======================
 
-  get("/zodiacs/aries", { :controller =>  "fire", :action =>  "ram" })
+  get("/zodiacs/sign", { :controller =>  "zodiac", :action =>  "horoscope" })
   get("/zodiacs/leo", { :controller => "fire", :action => "lion" })
   get("/zodiacs/sagittarius", { :controller => "fire", :action => "archer" })
   
@@ -29,25 +29,5 @@ Rails.application.routes.draw do
   # ==========================
     
   get("/roll/:number_of_dice/:how_many_sides", { :controller => "dice", :action => "dice_roll" })
-  get("/roll/2/6", { :controller => "dice", :action => "two_six" })
-  get("/roll/3/6", { :controller => "dice", :action => "three_six" })
-  get("/roll/4/6", { :controller => "dice", :action => "four_six" })
-  get("/roll/5/6", { :controller => "dice", :action => "five_six" })
-  get("/roll/6/6", { :controller => "dice", :action => "six_six" })
- 
-  get("/roll/1/4", { :controller => "dice", :action => "one_four" })
-  get("/roll/2/4", { :controller => "dice", :action => "two_four" })
-  get("/roll/3/4", { :controller => "dice", :action => "three_four" })
-  get("/roll/4/4", { :controller => "dice", :action => "four_four" })
-    
-  get("/roll/1/8", { :controller => "dice", :action => "one_eight" })
-  get("/roll/2/8", { :controller => "dice", :action => "two_eight" })
-  get("/roll/3/8", { :controller => "dice", :action => "three_eight" })
-  
-  get("/roll/1/10", { :controller => "dice", :action => "one_ten" })
-  get("/roll/2/10", { :controller => "dice", :action => "two_ten" })
 
-  get("/roll/1/20", { :controller => "dice", :action => "one_twenty" })
-  get("/roll/2/20", { :controller => "dice", :action => "two_twenty" })
-  get("/roll/3/20", { :controller => "dice", :action => "three_twenty" })
 end
